3f006b52c459f213ce01cbaf3e239c1807ba7d42
Utsav-Ghaskata <utsavprajapati117@gmail.com>
2026-05-20 18:21:28 +0530

    fix app dir path

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import os
 import sys
 
-
-APP_DIR = os.path.join(os.path.dirname(__file__), "mindcare_ai_project_content_website")
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
 os.chdir(APP_DIR)
 sys.path.insert(0, APP_DIR)
 
-import app  # noqa: E402,F401
+import app
